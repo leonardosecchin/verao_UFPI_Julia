0827261a4c827a70c50dc76d771291392008edaf
leonardosecchin <secchinleo@gmail.com>
2025-02-24 14:03:23 -0300

    versao 0

---
+++ codigo.jl
@@ -0,0 +1,16 @@
+# um vetor
+dados = [3.0;
+         4.0;
+         7.2
+         6.9]
+
+# uma função
+function teste()
+    println("Esta função está escrita no arquivo codigo.jl")
+    return
+end
+
+# outra função
+function teste2(d)
+    print("Vetor de entrada: ",d)
+end
